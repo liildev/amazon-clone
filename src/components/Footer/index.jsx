b443130b65766c29bdbc31d5582622c022ef2f8c
mansourov <mansurov.jr@mail.ru>
2022-09-20 20:50:53 +0500

    finished

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,26 +8,27 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <div className="footer">
-        <div className="footer__menu">
-          {footerLinks.map((footerlink) => (
-            <div key={footerlink.title} className="menu">
-              <h4>{footerlink.title}</h4>
-              <ul>
-                {footerlink.links.map((link) => (
-                  <li key={link.name}>
-                    <a href={link.link}>{link.name}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-        <div className="footer__block">
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-          <p>© 1996-2022, Amazon.com, Inc. or its affiliates</p>
-        </div>
+      <div className="footer__block">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+        <p>© 1996-2022, Amazon.com, Inc. or its affiliates</p>
+      </div>
+      
+      <div className="footer__menu">
+        {footerLinks.map((footerlink) => (
+          <div key={footerlink.title} className="menu">
+            <h4>{footerlink.title}</h4>
+            <ul>
+              {footerlink.links.map((link) => (
+                <li key={link.name}>
+                  <a href={link.link}>{link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/utils/products.js
@@ -0,0 +1,51 @@
+import { ipad, startUp, tv, mixer, watch, speaker } from "../assets";
+
+export const products = [
+  {
+    id: 12321341,
+    title:
+      "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback",
+    price: 11.96,
+    rating: 5,
+    image: startUp,
+  },
+  {
+    id: 49538094,
+    title:
+      "Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl",
+    price: 239.0,
+    rating: 4,
+    image: mixer,
+  },
+  {
+    id: 4903850,
+    title: "Samsung LC49RG90SSUXEN 49' Curved Smart Watch",
+    price: 199.99,
+    rating: 3,
+    image: watch,
+  },
+  {
+    id: 23445930,
+    title:
+      "Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric",
+    price: 98.99,
+    rating: 5,
+    image: speaker,
+  },
+  {
+    id: 3254354345,
+    title:
+      "New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)",
+    price: 598.99,
+    rating: 4,
+    image: ipad,
+  },
+  {
+    id: 90829332,
+    title:
+      "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440",
+    price: 1094.98,
+    rating: 4,
+    image: tv,
+  },
+];
